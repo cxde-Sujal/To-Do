16406aad1b6625dd9b91f60b43fc8a823f57268e
cxde-sujal <sujalsinha10@gmail.com>
2023-08-27 18:25:19 +0530

    Todo App

---
+++ todo.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("input-bar");
+const list = document.getElementById("list-container");
+function addTask() {
+    if(input.value==='')
+        alert("You must write something!!!");
+    else {
+        let li=document.createElement('li');
+        li.innerText=input.value;
+        list.appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    input.value=''
+}
+list.addEventListener("click",(e)=>{
+    // console.log(e.target.nodeName);
+    // console.log(e.target.parentElement);
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName ==="SPAN"){
+        // console.log(e.target.parentElement);
+        e.target.parentElement.remove();
+    }
+});
